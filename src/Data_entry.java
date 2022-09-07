18ee87f37306ad80c9e8862c521f810c66409e6d
Kalana Sudesh Gunaweera <105715377+KalanaGunaweera@users.noreply.github.com>
2022-09-07 23:50:54 +0530

    string

---
+++ src/Data_entry.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Data_entry {
+    private String string;
+    private String string2;
+    private String string3;
+
+    public String getString3() {
+        return string3;
+    }
+
+    public void setString3(String string3) {
+        this.string3 = string3;
+    }
+
+
+
+
+    public String getString2() {
+        return string2;
+    }
+
+    public void setString2(String string2) {
+        this.string2 = string2;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+
+
+
+
+    public void string_data_system(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome");
+        System.out.print("Please enter your text : ");
+        setString(input.nextLine());
+        while (true){
+            System.out.print("Please enter the word to replace : ");
+            setString3(input.next());
+            String[] arr= string.split(" ");
+            System.out.println(Arrays.toString(arr));
+            System.out.println("Replace with what? ");
+            setString2(input.next());
+
+
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i]!=string3){
+                    System.out.println("The word"+ string3+ " is not found ");
+                    break;
+                }else {
+                    System.out.println(string.replace(string3,string2));
+
+
+                }
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Data_entry data_entry=new Data_entry();
+        data_entry.string_data_system();
+        //System.out.println("Hello world!");
+    }
+}
